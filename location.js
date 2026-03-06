3d27667602c68e33e7c350141c75e79f2e70d3c0
adithyanap <adithyan130407@gmail.com>
2026-03-06 22:46:15 +0530

    api fetch corrected with error handling

--- location.js
+++ location.js
@@ -1,21 +1,36 @@
-
-
-function getLocation(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(success,error);
-    }else{
-        console.log("not supported");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        alert("Browser error");
     }
 }
-    function success(position){
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
 
 
-        console.log(latitude);
-        console.log(longitude);
 
+
+async function success(position) {
+    const latitude = await position.coords.latitude;
+    const longitude = await position.coords.longitude;
+    getAdress(latitude,longitude);
+
+}
+
+
+
+function error() {
+    alert("Location not found");
+}
+
+
+
+async function getAdress(lat,lon) {
+    try{
+    let url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
+    let responce = await fetch(url);
+    let data = await responce.json();
+    console.log(data);
+    }catch{
+        console.log("you are fucked up");
     }
-        function error(){
-        console.log("shit");
-    }
+}
