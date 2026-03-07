7a966b14d44723a8dc744efe8f0f7fdc08e51aff
adithyanap <adithyan130407@gmail.com>
2026-03-07 23:13:40 +0530

    weather-info class all completed

--- scripts.js
+++ scripts.js
@@ -27,8 +27,6 @@ class Time {
 
 
     }
-
-
     mer() {
         if (this.hour > 12) {
             this.merd = "pm";
@@ -37,12 +35,33 @@ class Time {
         }
         return (this.merd);
     }
+    returnTime(){
+        return [this.hour,this.min];
+    }
 
 }
+function changeNght(){
+    const time = new Time(now);
+    data = time.returnTime();
+    let image = 'moon.jpg';
+    if(data[0] >= 19){
+        
+        document.querySelector(".hero").style.background = `url("${image}") no-repeat center/cover`;
+        document.querySelector(".weather-info").style.background= "#080808";
+        document.querySelector(".sidebar").style.background= "#080808";
+        document.querySelector(".footer").style.background= "#080808";
+        document.querySelector("body").style.color= "#c3b5b5";
+        document.querySelector(".des").innerHTML= "It's a cloudy night";
+        document.querySelector(".sImage").src= "smallMoon.png";
+
+    }
+    
+}
+changeNght();
 
 const times = new Time(now);
 
-console.log(typeof now.getHours());
+
 document.querySelector(".date").innerHTML = date();
 document.querySelector(".time").childNodes[0].nodeValue = times.time();
 document.querySelector(".mer").innerHTML = times.mer();
