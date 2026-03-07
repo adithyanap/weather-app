af46a207fc111a268da8a88e524c9959eda1fe82
adithyanap <adithyan130407@gmail.com>
2026-03-07 12:29:55 +0530

    changed time function in time class better logic

--- scripts.js
+++ scripts.js
@@ -1,36 +1,44 @@
 let now = new Date();
-function date(){
+function date() {
     let day = now.getDay();
     let mon = now.getMonth();
     let year = now.getFullYear();
-    return (`${day+1}/${mon+1}/${year}`);
+    return (`${day + 1}/${mon + 1}/${year}`);
 
 }
 
 class Time {
-    constructor(now){
+    constructor(now) {
         this.hour = now.getHours();
         this.min = now.getMinutes();
     }
-    time(){
-        if (this.hour > 12){
-            if (this.hour < 10){
-                this.tHours = string(hour).padstart(2,"0");
-            }
-            this.tHours = this.hour-12;
-            return(`${this.tHours}:${this.min}`);
-        }else{
+    time() {
+        if (this.hour > 12) {
+            this.tHours = this.hour - 12;
+        }else if (this.hour === 0) {
+            this.tHours = 12;
+        } else if (this.hour === 11) {
             this.tHours = 11;
-            if (this.hour === 0 ){
-                this.tHours = 12;
-            }
-            return(`${this.tHours}:${this.min}`);
+        } else if (this.hour === 12) {
+            this.tHours = 12;
+        } else {
+            this.tHours = string(hour).padstart(2, "0");
         }
+        if (this.min < 10) {
+            this.tmin = string(min).padstart(2, "0");
+        } else {
+            this.tmin = this.min;
+        }
+        return (`${this.tHours}:${this.tmin}`);
+
+
     }
-    mer(){
-        if(this.hour > 12){
+
+
+    mer() {
+        if (this.hour > 12) {
             this.merd = "pm";
-        }else{
+        } else {
             this.merd = "am"
         }
         return (this.merd);
@@ -42,7 +50,7 @@ const times = new Time(now);
 
 
 document.querySelector(".date").innerHTML = date();
-console.log( times.time());
+console.log(times.time());
 
 document.querySelector(".time").childNodes[0].nodeValue = times.time();
 document.querySelector(".mer").innerHTML = times.mer();
